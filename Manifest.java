9fe11ab9ab9e757cafb3a980ea49fe89384a7de1
Sugiman <88069917+supersugiman@users.noreply.github.com>
2023-03-23 19:32:57 +0700

    Manifest

---
+++ Manifest.java
@@ -0,0 +1,9 @@
+package io.kodular.supersugiman.SIMPELSAK22;
+/* loaded from: classes.dex */
+public final class Manifest {
+
+    /* loaded from: classes.dex */
+    public static final class permission {
+        public static final String MAPS_RECEIVE = "io.kodular.supersugiman.SIMPELSAK22.permission.MAPS_RECEIVE";
+    }
+}
